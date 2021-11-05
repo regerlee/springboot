03c6366810558d2665c6ed741cf0dc23f44fcf89
regerlee <543200890@qq.com>
2021-11-05 17:52:39 +0800

    Add files via upload

---
+++ SpringbootProject/src/main/java/com/lee/SpringbootProject/rabbitmq/Demo1QueueConsumer.java
@@ -0,0 +1,21 @@
+package com.lee.SpringbootProject.rabbitmq;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RabbitListener(queues = "demoQueue")
+public class Demo1QueueConsumer {
+
+    /**
+     * 消息消费
+     * @RabbitHandler 代表此方法为接受到消息后的处理方法
+     */
+    @RabbitHandler
+    public void recieved(String msg) {
+        System.out.println("[demoQueue] demo1 recieved message: " + msg);
+    }
+
+}
---
+++ SpringbootProject/src/main/java/com/lee/SpringbootProject/rabbitmq/RabbitMqController.java
@@ -0,0 +1,18 @@
+package com.lee.SpringbootProject.rabbitmq;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RabbitMqController {
+
+    @Autowired
+    private RabbitProducer rabbitProducer;
+
+    @GetMapping("/sendDemoQueue")
+    public Object sendDemoQueue() {
+        rabbitProducer.sendDemoQueue();
+        return "success";
+    }
+}
